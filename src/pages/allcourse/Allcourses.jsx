cfb165fde2839f2bb3d2b6590524339b17b6c575
Your Name <adparas1@gmail.com>
2025-03-30 10:17:07 +0545

    Daily update -

--- src/pages/allcourse/Allcourses.jsx
+++ src/pages/allcourse/Allcourses.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Allcourses = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-4xl font-bold'>All Courses</h1>
       
     </div>
   )
